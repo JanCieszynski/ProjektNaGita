79b5f3a96900cabff305cc0742b22267ee7f59bf
JanCieszynski <janekcieszynski1@gmail.com>
2024-02-19 13:22:53 +0100

    .

--- projekt.php
+++ projekt.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,27 +10,27 @@
     <header>
         <h1>Nagłowek strony</h1>
     </header>
-    <div id="cointainer">
+    <div id="container">
         <?php
-        $db = new mysqli('local', 'root', '', 'cms');
-        $q = $db->prepare("SELECT post.id,post.imgUrl, post.title, 
-        post.timestamp, user.email 
-FROM `post` 
-INNER JOIN user ON post.author = user.ID;");
-$q->execute();
-$result = $q->get_result();
-while($row = $result->fetch_assoc()) {
-    echo '<div class="post-block">';
-    echo '<h2 class="post-title">'.$row['title'].'</h3>';
-    echo '<h3 class="post-author">'.$row['email'].'</h6>';
-    echo '<img src="'.$row['imgUrl'].'" alt="obrazek posta" class="post-image">';
-    echo '<p class="post-description">TODO: Opis posta</p>';
-    echo '<div class="post-footer">
-        <span class="post-meta">'.$row['timestamp'].'</span>
-        <span class="post-score">TODO: punkty</span>
-        </div>';
-    echo '</div>';
-}
+        $db = new mysqli('localhost', 'root', '', 'cms');
+        $q = $db->prepare("SELECT post.id, post.imgUrl, post.title, 
+                                    post.timestamp, user.email 
+                            FROM `post` 
+                            INNER JOIN user ON post.author = user.ID;");
+        $q->execute();
+        $result = $q->get_result();
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="post-block">';
+            echo '<h2 class="post-title">'.$row['title'].'</h3>';
+            echo '<h3 class="post-author">'.$row['email'].'</h6>';
+            echo '<img src="'.$row['imgUrl'].'" alt="obrazek posta" class="post-image">';
+            echo '<p class="post-description">TODO: Opis posta</p>';
+            echo '<div class="post-footer">
+                <span class="post-meta">'.$row['timestamp'].'</span>
+                <span class="post-score">TODO: punkty</span>
+                </div>';
+            echo '</div>'; 
+        }
         ?>
     </div>
 </body>
